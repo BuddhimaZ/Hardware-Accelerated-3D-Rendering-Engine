2295b41fa50426c84d959d443b0fe0ffb95bbdf1
Buddhima Zoysa <buddhimakaveeshwara@gmail.com>
2020-11-30 00:30:36 +0530

    Created a single header file that includes all the headersrequired to run CrappyECS.

---
+++ Hardware Accelerated 3D Rendering Engine/src/CrappyECS/CrappyECS.h
@@ -0,0 +1,13 @@
+#ifndef CRAPPYECS_H
+#define CRAPPYECS_H
+
+//Entity
+#include "Entity/CrappyEntity.h"
+
+//Base Component
+#include "Components/CrappyComponent.h"
+
+//All the components
+#include "Components/TransformComponent.h"
+
+#endif // !CRAPPYECS_H
