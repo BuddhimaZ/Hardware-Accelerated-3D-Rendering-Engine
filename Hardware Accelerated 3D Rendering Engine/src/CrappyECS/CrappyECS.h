02af116e22d6385fd6557a2f28a2dd8eb129dbb7
Buddhima Zoysa <buddhimakaveeshwara@gmail.com>
2020-11-30 11:50:47 +0530

    Added new components and scene headers

--- Hardware Accelerated 3D Rendering Engine/src/CrappyECS/CrappyECS.h
+++ Hardware Accelerated 3D Rendering Engine/src/CrappyECS/CrappyECS.h
@@ -4,10 +4,15 @@
 //Entity
 #include "Entity/CrappyEntity.h"
 
+//Scene
+#include "Entity/CrappyScene.h"
+
 //Base Component
 #include "Components/CrappyComponent.h"
 
 //All the components
 #include "Components/TransformComponent.h"
+#include "Components/ShittyCameraComponent.h"
+#include "Components/ShittyMeshComponent.h"
 
 #endif // !CRAPPYECS_H
