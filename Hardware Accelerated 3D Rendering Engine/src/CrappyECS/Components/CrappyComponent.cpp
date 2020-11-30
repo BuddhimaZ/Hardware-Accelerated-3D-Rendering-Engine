1955a11d6d22d9278d63cea73c832425df1e53c5
Buddhima Zoysa <buddhimakaveeshwara@gmail.com>
2020-11-30 11:47:41 +0530

    Added a bindable vector . And a function to add new Bindable to the vector from child components. Added Bind() function to bind all the Bindables of the list. Added GetRenderer() function to get a ptr to the Renderer object Made OnStart() and OnUpdate() functions none pure functions. Added a decontructor to delete all the bindables of the list

--- Hardware Accelerated 3D Rendering Engine/src/CrappyECS/Components/CrappyComponent.cpp
+++ Hardware Accelerated 3D Rendering Engine/src/CrappyECS/Components/CrappyComponent.cpp
@@ -7,7 +7,35 @@ CrappyComponent::CrappyComponent(
 {
 }
 
+CrappyComponent::~CrappyComponent()
+{
+	for (auto& pBindable : m_vecBindables) {
+		if (pBindable) delete pBindable;
+	}
+}
+
+void CrappyComponent::OnStart() {}
+
+void CrappyComponent::OnUpdate(const float& dt) {}
+
 const CrappyEntity* CrappyComponent::GetOwner() const
 {
 	return m_pOwner;
+}
+
+const void CrappyComponent::AddBindable(Bindable* pBindable)
+{
+	m_vecBindables.push_back(pBindable);
+}
+
+const void CrappyComponent::Bind() const
+{
+	for (const auto& pBindable : m_vecBindables) {
+		pBindable->Bind();
+	}
+}
+
+const Renderer* CrappyComponent::GetRenderer() const
+{
+	return m_pOwner->m_pRenderer;
 }
--- Hardware Accelerated 3D Rendering Engine/src/CrappyECS/Components/CrappyComponent.h
+++ Hardware Accelerated 3D Rendering Engine/src/CrappyECS/Components/CrappyComponent.h
@@ -3,6 +3,7 @@
 
 //Local Includes
 #include "../Entity/CrappyEntity.h"
+#include "../BindableSystem/BindableSystem.h"
 
 //Standard Includes
 #include <string>
@@ -10,18 +11,21 @@
 class CrappyComponent {
 	friend class CrappyEntity;
 public:
-	CrappyComponent(
-		const CrappyEntity* pOwner
-	);
+	CrappyComponent( const CrappyEntity* pOwner );
+	~CrappyComponent();
 
-protected:
-	virtual void OnStart() = 0;
-	virtual void OnUpdate(const float& dt) = 0;
+public:
+	virtual void OnStart();;
+	virtual void OnUpdate(const float& dt);;
 
 	const CrappyEntity* GetOwner() const;
+	const void AddBindable(Bindable* pBindable);
+	const void Bind() const;
+	const Renderer* GetRenderer() const;
 
 private:
 	const CrappyEntity* m_pOwner = nullptr;
+	std::vector<Bindable*> m_vecBindables = {};
 };
 
 #endif // !CRAPPYCOMPONENT_H
